c051eae61b8c1a832bb4b6b2289e42c4aaf6f6bc
Robert Schütz <dev@schuetz-co.de>
2020-11-11 19:03:56 +0100

    solve homework 5

--- lean/love05_inductive_predicates_homework_sheet.lean
+++ lean/love05_inductive_predicates_homework_sheet.lean
@@ -24,13 +24,63 @@ inductive term : Type
 /- 1.1 (1 point). Define an inductive predicate `is_app` that returns true if
 and only if its argument has an `app` constructor at the top level. -/
 
--- enter your definition here
+inductive is_app : term → Prop
+| app {t u : term} : is_app (term.app t u)
+
+lemma is_app_iff_is_app (s : term) :
+  is_app s ↔ ∃ t u : term, s = term.app t u :=
+begin
+  apply iff.intro,
+  {
+    intro h,
+    cases' s,
+    case var {
+      apply false.elim,
+      cases' h,
+    },
+    case lam {
+      apply false.elim,
+      cases' h,
+    },
+    case app : t u {
+      apply exists.intro t,
+      apply exists.intro u,
+      refl,
+    },
+  }, {
+    intro h,
+    apply exists.elim h,
+    intro s,
+    intro h,
+    apply exists.elim h,
+    intro u,
+    intro _,
+    simp [*],
+    apply is_app.app,
+  },
+end
 
 /- 1.2 (2 points). Define an inductive predicate `is_abs_free` that is true if
 and only if its argument is a λ-term that contains no λ-expressions. -/
 
--- enter your definition here
+inductive is_abs_free : term → Prop
+| var {x : string} : is_abs_free (term.var x)
+| app {t u : term} : is_abs_free t → is_abs_free u → is_abs_free (term.app t u)
 
+lemma lam_is_not_abs_free {x : string} {t : term} :
+  ¬is_abs_free (term.lam x t) :=
+begin
+  intro h,
+  cases' h,
+end
+
+lemma xy_is_abs_free {x y : string} :
+  is_abs_free (term.app (term.var x) (term.var y)) :=
+begin
+  apply is_abs_free.app,
+  apply is_abs_free.var,
+  apply is_abs_free.var,
+end
 
 /- ## Question 2 (6 points): Even and Odd
 
@@ -45,18 +95,19 @@ Lean definition below. The definition should distinguish two cases, like `even`,
 and should not rely on `even`. -/
 
 inductive odd : ℕ → Prop
--- supply the missing cases here
+| one             : odd 1
+| add_two {n : ℕ} : odd n → odd (n + 2)
 
 /- 2.2 (1 point). Give proof terms for the following propositions, based on
 your answer to question 2.1. -/
 
 lemma odd_3 :
   odd 3 :=
-sorry
+odd.add_two odd.one
 
 lemma odd_5 :
   odd 5 :=
-sorry
+odd.add_two (odd.add_two odd.one)
 
 /- 2.3 (2 points). Prove the following lemma by rule induction, as a "paper"
 proof. This is a good exercise to develop a deeper understanding of how rule
@@ -81,13 +132,32 @@ need not be justified if you think they are obvious (to humans), but you should
 say which key lemmas they depend on. You should be explicit whenever you use a
 function definition or an introduction rule for an inductive predicate. -/
 
--- enter your paper proof here
+/- We perform the proof by structural induction on `heven`.
+
+Case `zero`: The goal is `odd (0 + 1)`.
+By `linarith`, we have `0 + 1 = 1`. By `odd.one`, we have `odd 1`.
+We conclude `odd (0 + 1)`.
+
+Case `add_two`: The goal is `odd (n + 2 + 1)`.
+The induction hypothesis is `odd (n + 1)`.
+By `odd.add_two` and the induction hypothesis, we have `odd (n + 1 + 2)`.
+By `linarith`, we have `n + 1 + 2 = n + 2 + 1`.
+We conclude `odd (n + 2 + 1)`. -/
 
 /- 2.4 (1 point). Prove the same lemma again, but this time in Lean: -/
 
 lemma even_odd {n : ℕ} (heven : even n) :
   odd (n + 1) :=
-sorry
+begin
+  induction' heven,
+  case zero {
+    exact odd.one,
+  },
+  case add_two {
+    apply odd.add_two,
+    exact ih,
+  },
+end
 
 /- 2.5 (1 point). Prove the following lemma in Lean.
 
@@ -95,6 +165,17 @@ Hint: Recall that `¬ a` is defined as `a → false`. -/
 
 lemma even_not_odd {n : ℕ} (heven : even n) :
   ¬ odd n :=
-sorry
+begin
+  intro hodd,
+  induction' heven,
+  case zero {
+    cases' hodd,
+  },
+  case add_two {
+    apply ih,
+    cases' hodd,
+    exact hodd,
+  },
+end
 
 end LoVe
