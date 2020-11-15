27daec4e39f7f93d4b14ff1b0d0d24574971b774
Robert Schütz <dev@schuetz-co.de>
2020-11-15 11:44:53 +0100

    solve exercise sheet 5

--- lean/love05_inductive_predicates_exercise_sheet.lean
+++ lean/love05_inductive_predicates_exercise_sheet.lean
@@ -27,7 +27,10 @@ Hint: `cases'` is useful to reason about hypotheses of the form `even …`. -/
 
 @[simp] lemma odd_1 :
   odd 1 :=
-sorry
+begin
+  intro h,
+  cases' h,
+end
 
 /- 1.2. Prove that 3, 5, and 7 are odd. -/
 
@@ -39,7 +42,11 @@ lemma even_two_times :
   ∀m : ℕ, even (2 * m)
 | 0       := even.zero
 | (m + 1) :=
-  sorry
+  begin
+    simp [mul_add],
+    apply even.add_two,
+    exact even_two_times m,
+  end
 
 /- 1.4. Complete the following proof by rule induction.
 
@@ -55,7 +62,10 @@ begin
     apply exists.intro 0,
     refl },
   case add_two : k hek ih {
-    sorry }
+    apply exists.elim ih,
+    intros m h,
+    apply exists.intro (m + 1),
+    linarith, }
 end
 
 /- 1.6. Using `even_two_times` and `even_imp_exists_two_times`, prove the
@@ -63,7 +73,16 @@ following equivalence. -/
 
 lemma even_iff_exists_two_times (n : ℕ) :
   even n ↔ ∃m, n = 2 * m :=
-sorry
+iff.intro (
+  even_imp_exists_two_times n
+) (
+  assume h,
+  exists.elim h (
+    fix m,
+    assume n_eq_2m,
+    show even n, by simp [n_eq_2m, even_two_times m]
+  )
+)
 
 
 /- ## Question 2: Binary Trees
@@ -76,23 +95,61 @@ lemmas (e.g., `is_full_mirror`, `mirror_mirror`). -/
 
 lemma mirror_is_full {α : Type} :
   ∀t : btree α, is_full (mirror t) → is_full t :=
-sorry
+begin
+  intro t,
+  assume hfull,
+  rw ←mirror_mirror t,
+  apply is_full_mirror,
+  exact hfull,
+end
 
 /- 2.2. Define a `map` function on binary trees, similar to `list.map`. -/
 
 def btree.map {α β : Type} (f : α → β) : btree α → btree β
-| _ := sorry   -- remove this dummy case and enter the missing cases
+| btree.empty        := btree.empty
+| (btree.node a l r) := btree.node (f a) (btree.map l) (btree.map r)
 
 /- 2.3. Prove the following lemma by case distinction. -/
 
 lemma btree.map_eq_empty_iff {α β : Type} (f : α → β) :
   ∀t : btree α, btree.map f t = btree.empty ↔ t = btree.empty :=
-sorry
+begin
+  intro t,
+  cases' t,
+  case empty {
+    simp [btree.map],
+  },
+  case node {
+    simp [btree.map],
+  },
+end
 
 /- 2.4. Prove the following lemma by rule induction. -/
 
 lemma btree.map_mirror {α β : Type} (f : α → β) :
   ∀t : btree α, is_full t → is_full (btree.map f t) :=
-sorry
+begin
+  intro t,
+  intro hfull,
+  induction' hfull,
+  case empty {
+    simp [btree.map],
+    exact is_full.empty,
+  },
+  case node : _ l r _ _ hiff ihl ihr {
+    simp [btree.map],
+    apply is_full.node,
+    {
+      exact ihl f
+    },
+    {
+      exact ihr f
+    },
+    {
+      simp [btree.map_eq_empty_iff],
+      exact hiff,
+    },
+  },
+end
 
 end LoVe
